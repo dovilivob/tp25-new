3a1cd38d6dfe1fb38e6e7f63b4c8ca2e33332b84
dovilivob <david.iii.chiu@gmail.com>
2022-11-07 01:19:40 +0800

    get Date in getData.js

--- js/getData.js
+++ js/getData.js
@@ -7,7 +7,10 @@ const artistADDR = 'tz1Z8Xwm2qWnWWtL3MJ7T3A9uLmaQJiy8Uct';
 let OBJKT_ID = 0;
 let ownerList = 0;
 let votedList = 0;
-let ownerNum = 0
+let ownerNum = 0;
+let recentSoldDate = 0;
+let currentDate = 0;
+let expireDate = 0;
 
 const current_URL = window.location.href;
 console.log(current_URL);
@@ -16,29 +19,32 @@ const search = new URLSearchParams(window.location.search);
 
 const URL_viewer = search.get('viewer');
 const URL_objktId = search.get('objkt');
+
 if (URL_viewer != null) console.log(`Got viewer ADDR by url ${URL_viewer}`);
-else console.log("Can't get viewer ID by url")
+if (URL_objktId != null) console.log(`Got objkt ID by url ${URL_objktId}`);
+else console.log("Can't get viewer ID by url");
 
 GetDataSequence();
 
 async function GetDataSequence() {
-    if (URL_objktId != null) {
-        OBJKT_ID = URL_objktId;
-        console.log(`Got Token ID by url: ${URL_objktId}`)
-    } else {
-        await GetTokenDataByTag();
-        await GetVotedList();
-        console.log("Got Token ID by Tag: " + OBJKT_ID);
-    }
+    // if (URL_objktId != null) {
+    //     OBJKT_ID = URL_objktId;
+    //     console.log(`Got Token ID by url: ${URL_objktId}`);
+    // } else {
+    //     console.log("Got Token ID by Tag: " + OBJKT_ID);
+    // }
+    await GetTokenDataByTag();
+    // await GetVotedList();
     console.log("All Data is Ready");
+    await sleep(3000);
 }
 
-async function GetVotedList() {
-    let response = await fetch('https://tp25.2enter.art/assets/data.json')
-    let dataJson = await response.json();
-    votedList = dataJson.auths;
-    console.log(`Voted Owner List: ${votedList}`);
-}
+// async function GetVotedList() {
+//     let response = await fetch('https://tp25.2enter.art/assets/data.json');
+//     let dataJson = await response.json();
+//     votedList = dataJson.auths;
+//     console.log(`Voted Owner List: ${votedList}`);
+// }
 
 
 async function GetTokenDataByTag() {
@@ -46,9 +52,16 @@ async function GetTokenDataByTag() {
     let response = await fetch(apiUrl);
     let dataJson = await response.json();
     let objktJson = dataJson.tokens[0];
-    // console.log(objktJson)
+    // console.log(objktJson);
     OBJKT_ID = await objktJson.tokenId;
     ownerList = await objktJson.owners;
+    recentSoldDate = await objktJson.recentlySoldTime.split('T')[0];
+
+    recentSoldDate = new Date(recentSoldDate);
+    currentDate = new Date();
+    expireDate = new Date(recentSoldDate);
+    expireDate.setDate(expireDate.getDate() + 7);
+
     Object.keys(ownerList).forEach(key => {
         if (![artistADDR, 'KT1Dn3sambs7KZGW88hH2obZeSzfmCmGvpFo'].includes(key)) {
             ownerNum += ownerList[key];
@@ -56,4 +69,9 @@ async function GetTokenDataByTag() {
     })
     console.log(`Owner List: ${ownerList}`);
     console.log(`Owner Number: ${ownerNum}`);
+    console.log(`Recently Sold Time: ${recentSoldDate}`);
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
